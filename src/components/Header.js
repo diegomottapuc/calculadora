d3156e29a960957862b85560d10f1fd59ed954d8
diegomottapuc <diego.motta@sga.pucminas.br>
2025-05-04 20:53:23 -0300

    calculadora

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { Appbar } from 'react-native-paper';
+
+const Header = ({ title }) => {
+  return (
+    <Appbar.Header>
+      <Appbar.Content title={title} />
+    </Appbar.Header>
+  );
+};
+
+export default Header;
